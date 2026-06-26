9620086eda45a22c80aa2e5387e33949ad24e733
Darshan MT <darshanmalli93@gmail.com>
2026-06-26 04:41:51 +0000

    added configuration

---
+++ app/core/config.py
@@ -0,0 +1,11 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+
+    APP_NAME: str = "Address Book API"
+    DEBUG: bool = False
+    DATABASE_URL: str = "sqlite+aiosqlite:///./address_book.db"
+
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8",case_sensitive=False)       
+
+settings = Settings()
---
+++ app/core/database.py
@@ -0,0 +1,19 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine,async_sessionmaker
+from app.core.config import settings
+
+engine = create_async_engine(settings.DATABASE_URL, echo=settings.DEBUG)
+
+AsyncSessionLocal = async_sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
+
+async def get_db() -> AsyncSession:
+    async with AsyncSessionLocal() as session:
+        try:
+            yield session
+            await session.commit()
+        except Exception as e:
+            await session.rollback()
+            raise e
